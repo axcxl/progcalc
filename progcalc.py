1cc7c9c026041c1f46ec793da7877c6aed0d7972
axcxl <axcxl.dev@gmail.com>
2019-07-26 19:25:36 +0300

    Initial commit

---
+++ progcalc.py
@@ -0,0 +1,99 @@
+from guizero import App, TextBox, Text, Combo, Waffle, Box, PushButton
+
+class ProcCalc:
+    def __init__(self):
+        self.bin_sep = 8
+        self.value = 0
+
+        self.app = App(layout="grid")
+        self.top_box = Box(self.app, layout = "grid", grid = [0, 0])
+        self.bottom_box = Box(self.app, layout = "grid", grid = [0, 1])
+        self.right_box = Box(self.app, layout = "grid", grid = [1, 0, 1, 2])
+
+        # Create the text field to enter data
+        self.input = TextBox(self.top_box, width=25, grid=[0,0,2,1], command=self.process_input)
+        self.input.bg = "white"
+        self.input.text_size = 16
+
+        # Display the hex value
+        self.out_hex = Text(self.top_box, grid=[0,1,2,1], text="0x<waiting for valid input>")
+
+        # Display the binary value
+        self.out_bin = Text(self.top_box, grid=[1, 2], text="0b<waiting for valid input>")
+
+        # Display the binary value separation switch
+        self.in_binsep = Combo(self.top_box, grid=[0, 2], options=["4", "8", "16", "32"], command=self.process_binsep, selected="8")
+
+        # Prepare the waffle list
+        self.waffles = []
+
+
+        self.input.focus()
+        self.app.display()
+
+    def process_input(self, inp):
+        try:
+            self.value = int(self.input.value, 0)
+
+            self.display_bin()
+            self.display_hex()
+            self.display_waffle()
+        except ValueError:
+            return
+
+    def process_binsep(self, opt):
+        self.bin_sep = int(opt)
+        self.display_bin()
+
+    def process_waffle(self, event_data):
+        print("CLICKED", event_data.widget, event_data.x, event_data.y)
+
+    def display_hex(self):
+        self.out_hex.value = hex(self.value)
+
+    def display_bin(self):
+        # Get binary without 0b
+        outstring = bin(self.value)[2:]
+        n = len(outstring)
+
+        # Add zeros at beginning if needed
+        while(n % self.bin_sep != 0):
+            outstring = "0" + outstring
+            n += 1
+
+        # Split in groups of bin_sep
+        idx = self.bin_sep
+        while idx <= n:
+            outstring = outstring[0:idx] + " " + outstring[idx:]
+            idx += self.bin_sep + 1
+
+        self.out_bin.value = outstring
+
+    def display_waffle(self):
+        # Clear previos waffles in case the number changes
+        for w in self.waffles:
+            w.hide()
+            del w
+
+        # Display new ones, based on the binary representation of the number
+        x_coord = 0
+        for elem in self.out_bin.value.split(" "):
+            setattr(ProcCalc, "process_waffle_" + str(x_coord), self.process_waffle)
+            w = Waffle(self.bottom_box, height=len(elem), width= 1, grid = [x_coord, 3])
+            w.when_clicked = self.process_waffle
+            x_coord += 1
+
+            # Set the individual pixels
+            for i in range(0, len(elem)):
+                wp = w.pixel(0, i)
+                if elem[i] == "1":
+                    wp.color = "blue"
+
+            self.waffles.append(w)
+
+
+
+
+
+if __name__ == "__main__":
+    pc = ProcCalc()
